560fc7b0329530ebb26fd720541b72c5b7e85024
leeedongjaee <harold306122@gmail.com>
2025-06-23 19:35:50 +0900

    feat:웹소켓을 활용한 실시간 채팅 기능 구현

---
+++ src/main/java/dev/codehouse/backend/global/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package dev.codehouse.backend.global.config;
+
+import dev.codehouse.backend.global.exception.ChatExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new ChatExceptionHandler(), "/chat").setAllowedOrigins("*");
+    }
+}
